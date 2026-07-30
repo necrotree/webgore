2b2c2c90b9303bd05d6140d477da4a72e452ccc5
vexaden <vexaden@gmail.com>
2026-07-30 15:13:45 -0500

    Add 3-D Acceleration and Shortcuts

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <QApplication>
 #include <QIcon>
 #include <QLabel>
+#include <QKeySequence>
 #include <QLineEdit>
 #include <QMainWindow>
 #include <QPainter>
@@ -15,7 +16,19 @@
 #include <QToolBar>
 #include <QToolButton>
 #include <QUrl>
+#include <QWebPage>
 #include <QWebView>
+#include <QWebSettings>
+
+class BrowserPage final : public QWebPage {
+protected:
+    QString userAgentForUrl(const QUrl &) const override {
+        return QStringLiteral(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/126.0.0.0 Safari/537.36");
+    }
+};
 
 class BrowserWindow final : public QMainWindow {
 public:
@@ -136,6 +149,10 @@ public:
         forwardAction->setToolTip("Forward");
         reloadAction->setToolTip("Reload");
         homeAction->setToolTip("Home");
+        backAction->setShortcut(QKeySequence("Alt+Left"));
+        forwardAction->setShortcut(QKeySequence("Alt+Right"));
+        reloadAction->setShortcut(QKeySequence("Ctrl+R"));
+        homeAction->setShortcut(QKeySequence("Alt+Home"));
 
         addressBar_ = new QLineEdit(this);
         addressBar_->setObjectName("addressBar");
@@ -170,15 +187,24 @@ public:
             updateChrome();
         });
         connect(tabs_, &QTabWidget::tabCloseRequested, this, [this](int index) {
-            if (tabs_->count() == 1) {
-                currentWebView()->load(homeUrl());
-                return;
-            }
-
-            auto *closingPage = tabs_->widget(index);
-            tabs_->removeTab(index);
-            closingPage->deleteLater();
+            closeTab(index);
         });
+
+        addShortcut("Ctrl+T", [this] { createTab(homeUrl()); });
+        addShortcut("Ctrl+W", [this] { closeTab(tabs_->currentIndex()); });
+        addShortcut("Ctrl+L", [this] {
+            addressBar_->setFocus();
+            addressBar_->selectAll();
+        });
+        addShortcut("Ctrl+Tab", [this] {
+            tabs_->setCurrentIndex((tabs_->currentIndex() + 1) % tabs_->count());
+        });
+        addShortcut("Ctrl+Shift+Tab", [this] {
+            tabs_->setCurrentIndex((tabs_->currentIndex() + tabs_->count() - 1) % tabs_->count());
+        });
+        addShortcut("Ctrl++", [this] { currentWebView()->setZoomFactor(currentWebView()->zoomFactor() + 0.1); });
+        addShortcut("Ctrl+-", [this] { currentWebView()->setZoomFactor(currentWebView()->zoomFactor() - 0.1); });
+        addShortcut("Ctrl+0", [this] { currentWebView()->setZoomFactor(1.0); });
 
         createTab(homeUrl());
     }
@@ -240,6 +266,29 @@ private:
         return QUrl("https://www.google.com");
     }
 
+    void addShortcut(const char *shortcut, const std::function<void()> &handler) {
+        addShortcut(QKeySequence(QString::fromLatin1(shortcut)), handler);
+    }
+
+    void addShortcut(const QKeySequence &shortcut, const std::function<void()> &handler) {
+        auto *action = new QAction(this);
+        action->setShortcut(shortcut);
+        action->setShortcutContext(Qt::WindowShortcut);
+        connect(action, &QAction::triggered, this, handler);
+        addAction(action);
+    }
+
+    void closeTab(int index) {
+        if (tabs_->count() == 1) {
+            currentWebView()->load(homeUrl());
+            return;
+        }
+
+        auto *closingPage = tabs_->widget(index);
+        tabs_->removeTab(index);
+        closingPage->deleteLater();
+    }
+
     void navigateTo(const QUrl &url) {
         if (url.isValid()) {
             currentWebView()->load(url);
@@ -252,6 +301,7 @@ private:
 
     void createTab(const QUrl &url) {
         auto *webView = new QWebView(tabs_);
+        webView->setPage(new BrowserPage());
         const int index = tabs_->addTab(webView, "New Tab");
         tabs_->setCurrentIndex(index);
 
@@ -301,6 +351,15 @@ int main(int argc, char *argv[]) {
     QSslConfiguration sslConfiguration = QSslConfiguration::defaultConfiguration();
     sslConfiguration.setProtocol(QSsl::TlsV1_2OrLater);
     QSslConfiguration::setDefaultConfiguration(sslConfiguration);
+
+    QWebSettings *webSettings = QWebSettings::globalSettings();
+    webSettings->setAttribute(QWebSettings::AcceleratedCompositingEnabled, true);
+    webSettings->setAttribute(QWebSettings::Accelerated2dCanvasEnabled, true);
+    webSettings->setAttribute(QWebSettings::WebGLEnabled, true);
+    webSettings->setAttribute(QWebSettings::MediaSourceEnabled, true);
+    webSettings->setAttribute(QWebSettings::MediaEnabled, true);
+    webSettings->setAttribute(QWebSettings::WebAudioEnabled, true);
+    webSettings->setAttribute(QWebSettings::FullScreenSupportEnabled, true);
 
     BrowserWindow browser;
     browser.show();
